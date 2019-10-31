336d9c012435349e910c028a89e763f73e6d1a42
Dariush Abbasi <dariushem@yahoo.com>
2019-10-31 18:17:00 +0330

    more docker-compose aliases

---
+++ docker/aliases.zsh
@@ -0,0 +1,8 @@
+# docker-compose aliases
+alias d-c='docker-compose'
+alias dcb='docker-compose build'
+alias dcu='docker-compose up'
+alias dcud='docker-compose up -d'
+alias dcd='docker-compose down'
+alias dce='docker-compose exec'
+alias dcl='docker-compose logs'
--- zsh/aliases.zsh
+++
@@ -1,5 +0,0 @@
-# docker compose aliases
-alias dcb='docker-compose build'
-alias dcu='docker-compose up'
-alias dcud='docker-compose up -d'
-alias dcd='docker-compose down'
